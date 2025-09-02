5032cdc484efdbeae67291532ef75d9204997f5e
Jorge Tajan Acevedo <jorge_tajan@hotmail.com>
2025-09-01 21:56:18 -0500

    add persona model

---
+++ Model/Persona.java
@@ -0,0 +1,45 @@
+public class Persona {
+    // Atributos (propiedades)
+    private String nombre;
+    private int edad;
+    private String genero;
+
+    // Constructor
+    public Persona(String nombre, int edad, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+    // Métodos getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    // Método para mostrar información
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Género: " + genero);
+    }
+}
